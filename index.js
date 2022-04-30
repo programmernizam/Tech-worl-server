28d1bf50822e7998d21548d16ba2f302a4b68485
programmernizam <mdnizam12109@gmail.com>
2022-05-01 00:50:06 +0600

    added README.md

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+// MiddleWare
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s7i3i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("express").collection("user");
+  // perform actions on the collection object
+  console.log("db connect");
+  client.close();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(port, () => {
+  console.log("Listing port:", port);
+});
