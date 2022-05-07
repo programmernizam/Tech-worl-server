11d52d810b0501ae38872a40dd6f2d8a1c552973
programmernizam <mdnizam12109@gmail.com>
2022-05-07 21:09:52 +0600

    get item id

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ async function run(){
       const items = await cursor.toArray();
       res.send(items);
     })
+    app.get("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await itemCollection.findOne(query);
+      res.send(item);
+    });
 
     app.post('/items', async(req, res)=>{
       const newItem = req.body;
